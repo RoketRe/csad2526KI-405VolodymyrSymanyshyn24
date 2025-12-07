524b0ff779cdf67fa2434237f55c0537477fe331
reRoketR <volodymyr.rs7@icloud.com>
2025-12-07 18:41:59 +0200

    Add Google Test integration with math operations module and unit tests

---
+++ math_operations.h
@@ -0,0 +1,13 @@
+#ifndef MATH_OPERATIONS_H
+#define MATH_OPERATIONS_H
+
+/**
+ * Adds two integers and returns the result.
+ * 
+ * @param a First integer
+ * @param b Second integer
+ * @return Sum of a and b
+ */
+int add(int a, int b);
+
+#endif // MATH_OPERATIONS_H
